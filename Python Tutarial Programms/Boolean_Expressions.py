7527707ce5e1c7c4f50d13e137460ef61eba1fd3
GSidi <45037105+GSidi@users.noreply.github.com>
2021-09-30 14:39:36 +0300

    Add files via upload

---
+++ Python Tutarial Programms/Boolean_Expressions.py
@@ -0,0 +1,31 @@
+
+#Decesion Making and Loops
+
+#Part01 Boolean expressions
+
+x = 10
+y = 20
+
+print(x>y)
+print(x<y)
+print( x==y)
+print (x!=y)
+print(x>= 10)
+print(x >=20)
+print(x<= y)
+
+#Part02 Logical Operators
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+print (not True)
+print(not False)
+
+print(not (x != 10))
+print(not (x == 10))
